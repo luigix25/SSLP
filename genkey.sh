37669037b527269842bdd7171402da5bf2d8d9f6
luigix25 <luigix25@gmail.com>
2019-05-13 18:30:41 +0200

    Created Certificates with CA

--- genkey.sh
+++ genkey.sh
@@ -7,12 +7,14 @@ fi
 
 #Chiave Privata Server
 openssl genrsa -out keys/server_privkey.pem 3072
+openssl req -new -key keys/server_privkey.pem -out keys/certreq_server.pem
 
 #Chiave Pubblica Server
 openssl rsa -pubout -in keys/server_privkey.pem -out keys/server_pubkey.pem
 
 #Chiave Privata Client
 openssl genrsa -out keys/client_privkey.pem 3072
+openssl req -new -key keys/client_privkey.pem -out keys/certreq_client.pem
 
 #Chiave Pubblica Client
 openssl rsa -pubout -in keys/client_privkey.pem -out keys/client_pubkey.pem
